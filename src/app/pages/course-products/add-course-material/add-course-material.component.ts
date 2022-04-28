b4d97ade45cac74e01df4035b9dc26623d8fcadd
devendran <devendranvemula@gmail.com>
2022-04-28 14:37:56 +0530

    material video upload added with material upload function

--- src/app/pages/course-products/add-course-material/add-course-material.component.ts
+++ src/app/pages/course-products/add-course-material/add-course-material.component.ts
@@ -1,9 +1,11 @@
 import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { LoaderService } from 'src/app/services/loader/loader.service';
+import { VideoService } from 'src/app/services/video/video.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-add-course-material',
@@ -11,32 +13,82 @@ import { LoaderService } from 'src/app/services/loader/loader.service';
   styleUrls: ['./add-course-material.component.scss']
 })
 export class AddCourseMaterialComponent implements OnInit {
+  @ViewChild('materialFile') materialFileInput: ElementRef<HTMLInputElement> | undefined;
+  selectedMaterialFile: any;
   addMaterialForm: FormGroup;
   courseName = '';
   courseId = '';
-  categoryList = ['sddd', 'dbh', 'dhbcihs', 'hfiedhj']
+  fileUploadLoader = false;
+  preSignedUrl: any;
+  awsVideoUrl: any;
+
+
   constructor(
     private fb: FormBuilder,
     private loader: LoaderService,
     private toast: ToastrService,
     private route: ActivatedRoute,
     private location: Location,
+    private videoServe: VideoService,
   ) {
     this.addMaterialForm = this.fb.group({
       title: ['', [Validators.required]],
       description: ['', [Validators.required]],
-      category: ['', [Validators.required]],
-      // course: ['', [Validators.required]],
-      // file: ['', [Validators.required]]
+      // category: ['', [Validators.required]],
+      course: ['', [Validators.required]],
+      material: ['', [Validators.required]]
     })
     this.courseId = this.route.snapshot.paramMap.get('id') ?? '';
     this.courseName = this.route.snapshot.paramMap.get('course') ?? '';
   }
 
-  submit(): void {
-    const data = this.addMaterialForm.value;
-    data.course = this.courseId;
-    console.log(data);
+
+  // material change handle
+  handleFileSelection(): void {
+    const [file] = this.materialFileInput?.nativeElement?.files as any as File[];
+    if (file) {
+      this.selectedMaterialFile = file;
+    }
+  }
+
+  async uploadVideoMaterial(): Promise<void> {
+    try {
+      if (!this.selectedMaterialFile) {
+        this.toast.info("Please select the file");
+        return;
+      }
+      this.fileUploadLoader = true;
+      const data = {
+        fileName: this.selectedMaterialFile.name,
+        fileType: this.selectedMaterialFile.type,
+      }
+      this.loader.show();
+      this.preSignedUrl = await this.videoServe.getPreSignedUrl(data);
+      this.loader.hide();
+      const url = await this.videoServe.getAwsVideoUrl(this.preSignedUrl.url, this.selectedMaterialFile);
+      this.awsVideoUrl = environment.VIDEO_DOMAIN_URL + this.selectedMaterialFile.name
+      this.addMaterialForm.value["material"] = this.awsVideoUrl;
+      this.createMaterials();
+    } catch (error: any) {
+      console.log(error);
+      this.toast.error("Fail to upload")
+    } finally {
+
+    }
+  }
+
+  async createMaterials(): Promise<void> {
+    try {
+      this.loader.show();
+      const data = this.addMaterialForm.value;
+      data.course = this.courseId;
+      console.log(data);
+    } catch (error: any) {
+      console.log(error);
+      this.toast.error(error.error.message)
+    }finally{
+      this.loader.hide();
+    }
   }
 
   goBack(): void {
